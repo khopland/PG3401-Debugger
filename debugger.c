fe663f9283d848d58ddba53c4df696d86ef294c8
Kristoffer Larsen Hopland <24739853+BM0B0T@users.noreply.github.com>
2022-10-17 16:49:54 +0200

    added coments

--- debugger.c
+++ debugger.c
@@ -1,3 +1,9 @@
+/**
+ * @date:   2022-10-17 14:06:43
+ * @lastModifiedTime: 2022-10-17 16:37:57
+ * @License: Apache License 2.0, https://www.apache.org/licenses/LICENSE-2.0
+ */
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <time.h>
@@ -6,6 +12,7 @@ char glob_var = '1';
 #define _LENGTH_ 256
 
 void debugLogger(unsigned long ulErrorType, int iLine, const char *pszFile, const char *pszFormat, ...) {
+    // needs to be static to keep the value between calles.
     static FILE *fp = NULL, *sp;
     static int iCallCounter = 1;
 
@@ -14,6 +21,11 @@ void debugLogger(unsigned long ulErrorType, int iLine, const char *pszFile, cons
     char szFileName[_LENGTH_] = {0};
     time_t tTimeAndDate = 0;
 
+    va_list vaArgumentPointer = {0};
+    char szOutputString[_LENGTH_] = {0};
+    char *pszType = NULL;
+
+    // creates log file if its not created yet.
     if (fp == NULL) {
         tTimeAndDate = time(NULL) - 1601399931;
         snprintf(szFileName, _LENGTH_ - 1, "debug_%li.txt", tTimeAndDate);
@@ -26,20 +38,20 @@ void debugLogger(unsigned long ulErrorType, int iLine, const char *pszFile, cons
             int amountRes = fscanf(sp, "%[^\n\n]", buffer);
             glob_var = (amountRes > 0) ? buffer[0] : '0';
             fclose(sp);
+            sp = NULL;
         }
-
+        // early return if its not in debug and caller is calling from debug
         if (!(glob_var == '1' || glob_var == 'y' || glob_var == 'Y') && ulErrorType == 0) {
             return;
         }
     }
+    pszType = (ulErrorType == 1) ? "Error" : "Debug";
 
-    va_list vaArgumentPointer;
-    char szOutputString[_LENGTH_] = {0};
-    char *pszType = (ulErrorType == 1) ? "Error" : "Debug";
-
+    // reads all extra arguments and works like printf(...).
     va_start(vaArgumentPointer, pszFormat);
     vsnprintf(szOutputString, _LENGTH_ - 1, pszFormat, vaArgumentPointer);
     va_end(vaArgumentPointer);
 
+    // write to file the log statement.
     fprintf(fp, "%04i: %s: %s, at %s:%i\n", iCallCounter++, pszType, szOutputString, pszFile, iLine);
 }
